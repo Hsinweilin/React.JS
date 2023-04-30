ccdaa8b34d780ad4417fc3787a38f0c77b43490b
Hsinwei Lin <104602727+Hsinweilin@users.noreply.github.com>
2023-04-30 14:19:13 -0700

    Initial commit.

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import Message from './Message'//now we can use this as normal HTML component
+
+function App() {
+  return <div><Message></Message></div>
+}
+
+export default App
---
+++ src/Message.tsx
@@ -0,0 +1,8 @@
+
+function Message() {
+    //JSX: JavaScript XML
+    <h1>Hi</h1>
+    return <h1>Hello World</h1>
+}
+
+export default Message
