a9492a69bfcfd5bb3fae164bbeecd932192b561f
Hsinwei Lin <104602727+Hsinweilin@users.noreply.github.com>
2023-04-30 15:56:35 -0700

    Complete ListGroup Component

--- src/Components/ListGroup.tsx
+++ src/Components/ListGroup.tsx
@@ -1,12 +1,20 @@
+//import { Fragment } from "react";
+
 function ListGroup() {
+  let items = ["New York", "San Francisco", "Tokyo", "London"];
+
   return (
-    <ul className="list-group">
-      <li className="list-group-item">An item</li>
-      <li className="list-group-item">A second item</li>
-      <li className="list-group-item">A third item</li>
-      <li className="list-group-item">A fourth item</li>
-      <li className="list-group-item">And a fifth one</li>
-    </ul>
+    //can't return more than one element
+    //empty bracket in react means a Fragment component
+    <>
+      <h1>List</h1>
+      {items.length === 0 && <p>No item found</p>}
+      <ul className="list-group">
+        {items.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
+      </ul>
+    </>
   );
 }
 
