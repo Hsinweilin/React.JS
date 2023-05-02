d968eaf0ee465e80950777c6073c76bbe9cbbe66
Hsinwei Lin <104602727+Hsinweilin@users.noreply.github.com>
2023-05-02 13:05:24 -0700

    Add Hook(useState) for list group item

--- src/Components/ListGroup.tsx
+++ src/Components/ListGroup.tsx
@@ -1,7 +1,14 @@
 //import { Fragment } from "react";
+import { MouseEvent, useState } from "react";
 
 function ListGroup() {
   let items = ["New York", "San Francisco", "Tokyo", "London"];
+
+  //Hook
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+
+  //Event Handler
+  //const handleClick = (event: MouseEvent) => console.log(event);
 
   return (
     //can't return more than one element
@@ -10,8 +17,20 @@ function ListGroup() {
       <h1>List</h1>
       {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
-        {items.map((item) => (
-          <li key={item}>{item}</li>
+        {items.map((item, index) => (
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active:"
+                : "list-group item"
+            }
+            key={item}
+            onClick={() => {
+              setSelectedIndex(index);//selectedIndex = index
+            }} //handleClick
+          >
+            {item}
+          </li>
         ))}
       </ul>
     </>
